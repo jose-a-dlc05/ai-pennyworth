f18544f310f081875919c0cfc5675b9781be577a
Jose De La Cruz <jose.delcruz0490@icloud.com>
2024-09-22 00:52:10 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Form from './section/Form';
+
+function App() {
+	return (
+		<>
+			<div className='container'>
+				<Form />
+			</div>
+		</>
+	);
+}
+
+export default App;
---
+++ src/section/Form.tsx
@@ -0,0 +1,11 @@
+import './Form.css';
+const Form = () => {
+	return (
+		<>
+			<div className='form'>Form</div>
+			<div className='form-img'>Img</div>
+		</>
+	);
+};
+
+export default Form;
